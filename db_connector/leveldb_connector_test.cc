e90cde7d6e135a912a6a6054e3a53d5a36c94cf2
atlasmir <hi@xfzhao.com>
2022-10-28 22:40:30 +0800

    Init

---
+++ db_connector/leveldb_connector_test.cc
@@ -0,0 +1,12 @@
+//
+// Created by xiaofzhao4 on 10/28/2022.
+//
+
+#include "db_connector/connector.h"
+#include "gtest/gtest.h"
+
+namespace smvkv {
+
+TEST(Connector, Empty) { Connector connector; }
+
+}  // namespace smvkv
---
+++ include/db_connector/connector.h
@@ -0,0 +1,19 @@
+//
+// Created by xiaofzhao4 on 10/28/2022.
+//
+
+#ifndef SMVKV_DB_CONNECTOR_CONNECTOR_H_
+#define SMVKV_DB_CONNECTOR_CONNECTOR_H_
+
+namespace smvkv {
+
+class Connector {
+ public:
+  Connector() = default;
+  virtual ~Connector() = default;
+};
+
+}  // namespace smvkv
+
+
+#endif  // SMVKV_DB_CONNECTOR_CONNECTOR_H_
